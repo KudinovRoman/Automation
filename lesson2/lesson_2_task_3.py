abab32d61aa5981b9f99327d6287f49700beef6b
KudinovRoman <foma.kiniaev007@gmail.com>
2024-06-10 22:51:53 +0700

    add second homework

---
+++ lesson2/lesson_2_task_3.py
@@ -0,0 +1,13 @@
+import math
+
+line = float(input("Введите длину стороны: "))
+
+def square (x):
+    return math.ceil(x*x)
+
+print("Площадь квдрата равна:", square(line))
+
+
+
+
+
---
+++ lesson2/lesson_2_task_8.py
@@ -0,0 +1,9 @@
+
+lst = [18, 14, 10, 6, 2]
+
+my_list = []
+
+for i in range(18, 1, -4):
+    my_list.append(i)
+
+print(my_list)
